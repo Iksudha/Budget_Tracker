9678cf7c80919744db1446ac6f9d3fd78619ed70
IT19056562 <68426609+Sona-Nizer@users.noreply.github.com>
2022-10-28 23:52:19 +0530

    Create ListFragment.kt

---
+++ app/src/main/java/com/android/taskReminder/list/ListFragment.kt
@@ -0,0 +1,168 @@
+package com.bignerdranch.android.to_dolist.fragments.list
+
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.to_dolist.databinding.FragmentListBinding
+import com.bignerdranch.android.to_dolist.R
+import com.bignerdranch.android.to_dolist.data.SortOrder
+import com.bignerdranch.android.to_dolist.viewmodel.TodoViewModel
+import com.bignerdranch.android.to_dolist.model.Todo
+import com.bignerdranch.android.to_dolist.utils.onQueryTextChanged
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+private const val TAG = "ListFragment"
+
+class ListFragment : Fragment(), TodoAdapter.OnItemClickListener {
+    private var _binding : FragmentListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var mTodoViewModel: TodoViewModel
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter : TodoAdapter
+    private var todosList = emptyList<Todo>()
+    private var todo = listOf<Todo>()
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment with ViewBinding style
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+
+        // this tells our activity/fragment that we have a menu_item it should respond to it.
+        setHasOptionsMenu(true)
+
+        adapter = TodoAdapter(requireContext(), this)
+
+        recyclerView = binding.recyclerViewTodo
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.setHasFixedSize(true)
+
+        /**
+         *  updates our recyclerView with the new "observed" changes in our database through our adapter
+         */
+        // TodoViewModel
+        mTodoViewModel = ViewModelProvider(this)[TodoViewModel::class.java]
+        mTodoViewModel.tasks.observe(viewLifecycleOwner) { todos ->
+            adapter.submitList(todos)
+            todosList = todos
+        }
+
+        // Add Task Button
+        binding.fbAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+        return binding.root
+    }
+
+    override fun onCheckBoxClick(todo: Todo, isChecked: Boolean) {
+        mTodoViewModel.onTaskCheckedChanged(todo, isChecked)
+    }
+
+    override fun onItemDelete(todo: Todo) {
+        mTodoViewModel.deleteTask(todo)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.fragment_list, menu)
+
+        val search = menu.findItem(R.id.todo_search)
+        val searchView = search.actionView as SearchView
+
+        searchView.onQueryTextChanged { querySearch ->
+            mTodoViewModel.searchQuery.value = querySearch
+        }
+
+        // we want the state of the hideCompleted to be the same with the one in our dataStore when the app starts and not when
+        // it is clicked
+        viewLifecycleOwner.lifecycleScope.launch {
+            menu.findItem(R.id.action_hide_completed_tasks).isChecked = mTodoViewModel.preferencesFlow.first().hideCompleted
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId)  {
+            R.id.sort_by_name -> {
+                mTodoViewModel.onSortOrderSelected(SortOrder.BY_NAME)
+                true
+            }
+            R.id.sort_by_date -> {
+                mTodoViewModel.onSortOrderSelected(SortOrder.BY_DATE)
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                mTodoViewModel.onHideCompletedClick(item.isChecked)
+                true
+            }
+            R.id.del_selected_tasks -> {
+                deleteSelectedUsers()
+                 true
+            }
+            R.id.del_all_tasks -> {
+                deleteAllTasks()
+                true
+            }
+            R.id.about -> {
+                findNavController().navigate(R.id.action_listFragment_to_aboutFragment)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    // function to delete all of our Tasks
+    private fun deleteAllTasks() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") {_,_->
+            mTodoViewModel.deleteAllTasks()
+            Toast.makeText(requireContext(), "All tasks have been successfully deleted!", Toast.LENGTH_LONG).show()
+        }
+        builder.setNegativeButton("No") {_,_-> }
+        builder.setTitle("Confirm Deletion")
+        builder.setIcon(R.drawable.ic_warning)
+        builder.setMessage("Are you sure you want to delete all Tasks?")
+        builder.create().show()
+    }
+
+    // function to delete only selected Tasks
+    @SuppressLint("NotifyDataSetChanged")
+    private fun deleteSelectedUsers() {
+        val builder = AlertDialog.Builder(requireContext())
+        // Our todos that have been marked completed by the checkBox
+        val finishedTodos = todosList.filter { it.completed }
+        builder.setPositiveButton("Yes") {_,_->
+            finishedTodos.forEach { todos ->
+                mTodoViewModel.deleteCompletedTasks(todos.id.toLong())
+            }
+            Toast.makeText(requireContext(), "Completed tasks successfully deleted!", Toast.LENGTH_LONG).show()
+        }
+        builder.setNegativeButton("No") {_,_-> }
+        builder.setTitle("Confirm Deletion")
+        builder.setMessage("Are you sure you want to delete all completed Tasks?")
+        builder.create().show()
+        Log.i(TAG , "Our todos list size is ${finishedTodos.size}")
+    }
+
+    // We want to leave no trace of our Binding class Reference to avoid memory leaks
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
